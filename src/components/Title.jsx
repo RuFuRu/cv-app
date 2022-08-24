b8c8fde2cadf8b29f94440ec9111b8a3a464ebda
RuFuRu <94857865+RuFuRu@users.noreply.github.com>
2022-08-24 18:17:21 +0200

    added title component

---
+++ src/components/Title.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Title extends Component {
+  render() { 
+    return (
+      <header>
+        <h1>CV Creator</h1>
+      </header>
+    );
+  }
+}
+ 
+export default Title;
