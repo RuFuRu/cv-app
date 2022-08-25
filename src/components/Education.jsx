c75f265772150ad50ea93b5beb00946df334a53d
RuFuRu <94857865+RuFuRu@users.noreply.github.com>
2022-08-25 22:01:23 +0200

    implemented form for Education

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -22,13 +22,20 @@ class Education extends Component {
         {this.state.counter % 2 !== 0 &&
           <form>
             <label htmlFor="uniName">Name of the University:</label>
-            <input type="text" id='uniName' name='uniName' required/>
-            <label htmlFor="time">Timeperiod</label>
-            <input type="text" id ="time" name="time"
+            <input type="text" id='uniName' name='uniName' required/><br />
+            <label htmlFor="timeFrom">From:</label>
+            <input type="text" id ="timeFrom" name="timeFrom"
              placeholder="10-1970 (month-year)"
              pattern='(\d){2}-(\d){4}'
-             required />
-             <button>Save</button>
+             required /><br />
+            <label htmlFor="timeTo">To:</label>
+            <input type="text" id='timeTo' name='timeTo'
+             placeholder="10-1971 (month-year)"
+             pattern= '(\d){2}-(\d){4}'
+             required /><br />
+            <label htmlFor="qualify">Qualifications:</label>
+            <input type="text" id='qualify' name='qualify'/><br />
+            <button>Save</button>
           </form>
         }
       </div>
