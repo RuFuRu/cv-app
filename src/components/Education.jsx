ffc9d0dcab3466128f6da2eae753856f4867f9e7
RuFuRu <94857865+RuFuRu@users.noreply.github.com>
2022-08-25 21:56:23 +0200

    implemented regex in Education

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,13 +1,36 @@
 import React, { Component } from 'react';
 
 class Education extends Component {
-  state = {  } 
+  state = { 
+    counter: 0
+   } 
+
+  handleCounter = () => {
+    this.setState({counter: this.state.counter + 1})
+  } 
+  
   render() { 
     return (
       <div>
         <h3>Add Educational Experience</h3>
-        <button type='button'>Add</button>
-        <button type='button'>Delete</button>
+        {this.state.counter % 2 === 0 &&
+          <>
+          <button type='button' onClick={this.handleCounter}>Add</button>
+          <button type='button'>Delete</button>
+          </>
+        }
+        {this.state.counter % 2 !== 0 &&
+          <form>
+            <label htmlFor="uniName">Name of the University:</label>
+            <input type="text" id='uniName' name='uniName' required/>
+            <label htmlFor="time">Timeperiod</label>
+            <input type="text" id ="time" name="time"
+             placeholder="10-1970 (month-year)"
+             pattern='(\d){2}-(\d){4}'
+             required />
+             <button>Save</button>
+          </form>
+        }
       </div>
     );
   }
