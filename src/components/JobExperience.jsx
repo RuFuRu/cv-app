cda33a7adce7fb35c15b4506d3b910b89eb77cc1
RuFuRu <94857865+RuFuRu@users.noreply.github.com>
2022-08-26 11:43:19 +0200

    made JobExperience functional component

--- src/components/JobExperience.jsx
+++ src/components/JobExperience.jsx
@@ -1,16 +1,26 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
-class JobExperience extends Component {
-  state = {  } 
-  render() { 
-    return (
-      <div>
-        <h3>Add Practical Experience</h3>
-        <button type='button'>Add</button>
-        <button type='button'>Delete</button>
-      </div>
-    );
+
+function JobExperience() {
+  const [counter, setCounter] = useState(0);
+
+  const handleCounter = () => {
+    setCounter(counter + 1)
   }
+
+  return (
+    <div>
+      <h3>Add Practical Experience</h3>
+      {counter % 2 === 0 &&
+        <>
+        <button type='button' onClick={handleCounter}>Add</button>
+        <button type='button'>Delete</button>
+        </>
+      }
+      
+    </div>
+  );
+
 }
  
 export default JobExperience;
