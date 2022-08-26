bf2ffca04824fae9c18583c1a6e9cf8eb5e442a4
RuFuRu <94857865+RuFuRu@users.noreply.github.com>
2022-08-26 11:51:02 +0200

    added form in JobExperience

--- src/components/JobExperience.jsx
+++ src/components/JobExperience.jsx
@@ -17,7 +17,31 @@ function JobExperience() {
         <button type='button'>Delete</button>
         </>
       }
-      
+      {counter % 2 !== 0 &&
+        <form>
+          <label htmlFor="company">Name of the Company:</label>
+          <input type="text" name="company" id="company"
+           required/><br />
+          <label htmlFor="jobTitle">Job Title:</label>
+          <input type="text" name='jobTitle' id='jobTitle'
+           required/><br />
+          <label htmlFor="from">From:</label>
+          <input type="text" name='from' id='from'
+           placeholder="10-1970 (month-year)"
+           pattern='(\d){2}-(\d){4}'
+           required /><br />
+          <label htmlFor="to">To:</label>
+          <input type="text" name='to' id='to'
+           placeholder="10-1971 (month-year)"
+           pattern='(\d){2}-(\d){4}'
+           required /><br />
+          <label htmlFor="jobDesc">Job Description:</label><br />
+          <textarea name="jobDesc" id="jobDesc" cols="30" rows="10">
+            Please describe your work at the company
+          </textarea><br />
+          <button type='submit'>Save</button>
+        </form>
+      }
     </div>
   );
 
